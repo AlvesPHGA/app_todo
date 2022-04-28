d85c1a0ac2e18f7fbf9cbb8c199b2cc362a41c2a
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-04-28 11:59:33 -0300

    updated proj: change_color function done

--- script/modules/change-color.js
+++ script/modules/change-color.js
@@ -14,8 +14,21 @@ export default function change_color() {
   }
 
   function change_back_sub() {
+    const body = document.querySelector("body");
     const subheader = document.querySelector(".subheader");
+    const input = subheader.querySelector(".new_todo");
 
-    subheader.classList.toggle("__active");
+    const ul = document.querySelector("ul");
+
+    const utils = [body, subheader, input, ul];
+
+    utils.forEach((i) => {
+      i.classList.toggle("__active");
+    });
+  }
+
+  function change_body_backColor() {
+    const body = document.querySelector("body");
+    body.classList.toggle("__active");
   }
 }
