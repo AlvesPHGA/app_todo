8b843b63ad1ccce09ba7415747cb2b66436cd808
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-04-28 15:22:00 -0300

    updated proj: add active-status function

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
 import change_color from "./modules/change-color.js";
+import active_status from "./modules/active-status.js";
 
 change_color();
+active_status();
---
+++ script/modules/active-status.js
@@ -0,0 +1,17 @@
+export default function active_status() {
+  const active = document.querySelectorAll("[data-status]");
+
+  const link_active = document.querySelector(".status");
+
+  link_active.addEventListener("click", (e) => {
+    status_active(e);
+  });
+
+  function status_active(e) {
+    active.forEach((i) => {
+      i.classList.remove("__active");
+    });
+
+    e.target.classList.add("__active");
+  }
+}
