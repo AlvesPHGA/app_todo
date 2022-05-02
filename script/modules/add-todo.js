71ce39ade95c622b9a319717fa3d440f883c7e3c
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-05-02 13:33:02 -0300

    update proj: updated list-item and add-todo function

--- script/modules/add-todo.js
+++ script/modules/add-todo.js
@@ -1,31 +1,54 @@
 export default function add_todo() {
   const form = document.querySelector("form");
+  const c_list = document.querySelector(".clean_list");
   const list = [];
 
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", event);
+  c_list.addEventListener("click", all_clean);
+
+  function event(e) {
     e.preventDefault();
+    init();
+  }
+
+  function init() {
     const item = form.querySelector("[data-newTodo]").value;
 
     clean();
-    // add_item(item);
-  });
+    add_item(item);
+  }
 
   function clean() {
     form.reset();
   }
 
-  //   function add_item(item) {
-  //     let ul = document.querySelector("ul");
-  //     const li = document.createElement("li");
-  //     list.push(item);
+  function add_item(item) {
+    list.push(item);
 
-  //     ul = `<li data-todo>
-  //     <input type="radio" name="todo" id="">
-  //     <span>Complete online JavaScript course</span>
-  //     </li>`;
+    const ul = document.querySelector("ul");
 
-  //     list.map((i) => {});
+    const li = document.createElement("li");
+    const span = document.createElement("span");
 
-  //     console.log(ul);
-  //   }
+    span.textContent = item;
+
+    li.setAttribute("data-todo", "");
+    li.setAttribute("name", "todo");
+
+    li.appendChild(span);
+
+    ul.appendChild(li);
+  }
+
+  function all_clean() {
+    const ul = document.querySelector("ul");
+    const last_li = document.querySelector(".last-li");
+    const li = ul.querySelectorAll("li");
+    // const hasAttr = li.hasAttribute("data-todo");
+
+    // if (ul.firstChild.getAttribute("[data-todo]"))
+    //   console.log(ul.removeChild(ul.firstChild));
+
+    console.log((li.dataset = todo));
+  }
 }
